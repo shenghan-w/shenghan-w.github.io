22cf7437b8498260cd6ec2fd6d81e8b64afdd2f3
shenghan-w <shenghan.wang92@gmail.com>
2025-10-15 13:36:54 -0600

    Rework of portfolio
    rebuilt with Astro Starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,75 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapidePlugin from 'starlight-theme-rapide';
+import starlightUtils from "@lorenzo_lewis/starlight-utils";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'http://shenghan-w.github.io',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+
+			// Plugin Config
+			plugins: [
+				starlightThemeRapidePlugin(),
+				starlightUtils({
+					navLinks: {
+						leading: { useSidebarLabelled: "leadingNavLinks" },
+					},
+				}),
+			],
+			
+			// Title
+			title: "Shen's Stuff",
+
+			// Settings
+			tableOfContents: {
+				minHeadingLevel: 2,
+				maxHeadingLevel: 4,
+			},
+
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/shenghan-w' },
+				{icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/shenghan-wang-30631573/' },
+			],
+
+			pagination: false,
+
+			favicon: '/favicon.svg',
+
+			expressiveCode: {
+				themes: ['solarized-dark', 'solarized-light']
+			},
+			
+			// Customs
+			routeMiddleware: './src/routeData.ts',
+			disable404Route: true,
+			
+			components: {
+				Hero: './src/components/CustomHero.astro',
+			},
+
+			// Sidebar Config
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'leadingNavLinks',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Security Stuff', slug: 'security' },
+						{ label: 'Tech Writing Stuff', slug: 'tech-writing' },
+						{ label: 'Climbing Stuff', slug: 'climbing' },
 					],
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				
+				{ label: 'About My Security Stuff', autogenerate: { directory: 'security/about' }},
+				{ label: 'HackTheBox Write-Ups', autogenerate: { directory: 'security/htb' }},
+				{ label: 'Red Team Notes', autogenerate: { directory: 'security/cheatsheets' }},
+
+				{ label: 'About My Tech Writing', autogenerate: { directory: 'tech-writing/about' }},
+				{ label: 'Progression Docs', autogenerate: { directory: 'tech-writing/progression' }},
+				
+				{ label: 'About My Climbing', autogenerate: { directory: 'climbing/about' }},
+				{ label: 'Outdoor Bouldering', autogenerate: { directory: 'climbing/projects' }},
 			],
 		}),
 	],
